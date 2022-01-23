afa4b09c183e466dfdd8c9094ff770df9e699aae
HadeelDaragmeh158 <hadeelmohammad158@gmail.com>
2022-01-23 22:31:43 +0200

    task6add

--- js/app.js
+++ js/app.js
@@ -39,3 +39,56 @@ if (confirm(conf) == true) {
     else
         alert("Welcome " = nname);
 }
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+var q1;
+var q2;
+var q3;
+var Rarray = new array();
+
+
+function AskUser(q1) {
+    let q1 = prompt("Do you love Mansaf?");
+    IsEmpty(q1);
+    return q1;
+}
+function IsEmpty(q1) {
+    if (!EmptyEnter(q1)) {
+        alert = "Invalid";
+    }
+    if (!EmptyEnter(q1)) {
+        alert = "Invalid";
+    }
+    if (!EmptyEnter(q1)) {
+        alert = "Invalid";
+    }
+}
+
+function EmptyEnter(p1) {
+    if (p1 != null) {
+        return true;
+        // document.getElementById("demo").innerHTML =
+        // "Hello " + person + "! How are you today?";
+    }
+    else return false;
+}
+
+function PutResultInArray(p1, p2, p3) {
+    Rarray = [p1, p2, p3];
+}
+
+function PrintArray() {
+for(var x=0;x<=3;x++)
+{
+    console.log(Rarray[x]);
+}
+}
+
+q1=AskUser(q1);
+q2=AskUser(q2);
+q3=AskUser(q3);
+PutResultInArray(p1, p2, p3);
+PrintArray();
